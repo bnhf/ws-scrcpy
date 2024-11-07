b5aea03d19322828b7d9b08f36bbb5aa41cffd07
Scott Ueland <41088895+bnhf@users.noreply.github.com>
2024-11-07 16:44:30 -0700

    Support for 8 virtual tuners

--- docker-start.sh
+++ docker-start.sh
@@ -151,12 +151,12 @@ createM3Us() {
 # Fix hostanme resolution, connect adb devices, copy scripts and M3U files as needed, start ws-scrcpy and ah4c
 main() {
 
-  fixTunerDNS $TUNER1_IP $TUNER2_IP $TUNER3_IP $TUNER4_IP
-  fixEncoderDNS $ENCODER1_URL $ENCODER2_URL $ENCODER3_URL $ENCODER4_URL
-  adbConnections $TUNER1_IP $TUNER2_IP $TUNER3_IP $TUNER4_IP
+  fixTunerDNS $TUNER1_IP $TUNER2_IP $TUNER3_IP $TUNER4_IP $TUNER5_IP $TUNER6_IP $TUNER7_IP $TUNER8_IP $TUNER9_IP
+  fixEncoderDNS $ENCODER1_URL $ENCODER2_URL $ENCODER3_URL $ENCODER4_URL $ENCODER5_URL $ENCODER6_URL $ENCODER7_URL $ENCODER8_URL $ENCODER9_URL
+  adbConnections $TUNER1_IP $TUNER2_IP $TUNER3_IP $TUNER4_IP $TUNER5_IP $TUNER6_IP $TUNER7_IP $TUNER8_IP $TUNER9_IP
   checkScripts prebmitune.sh bmitune.sh stopbmitune.sh isconnected.sh keep_alive.sh reboot.sh createm3u.sh common.sh
-  checkM3Us directv.m3u dtvdeeplinks.m3u dtvosprey.m3u dtvstream.m3u foo-fighters.m3u fubo.m3u hulu.m3u livetv.m3u nbc.m3u npo.m3u pbs-seatac.m3u pbs-worcester.m3u silicondust.m3u sling.m3u spectrum.m3u xfinity.m3u youtubetv_shield.m3u youtubetv.m3u
-  createM3Us $TUNER1_IP $TUNER2_IP $TUNER3_IP $TUNER4_IP
+  checkM3Us directv.m3u dtvdeeplinks.m3u dtvosprey.m3u dtvstream.m3u dtvstreamdeeplinks.m3u foo-fighters.m3u fubo.m3u hulu.m3u livetv.m3u nbc.m3u npo.m3u pbs-seatac.m3u pbs-worcester.m3u silicondust.m3u sling.m3u spectrum.m3u xfinity.m3u youtubetv_shield.m3u youtubetv.m3u
+  createM3Us $TUNER1_IP $TUNER2_IP $TUNER3_IP $TUNER4_IP $TUNER5_IP $TUNER6_IP $TUNER7_IP $TUNER8_IP $TUNER9_IP
   npm start --prefix ws-scrcpy &
   ./ah4c
 }
